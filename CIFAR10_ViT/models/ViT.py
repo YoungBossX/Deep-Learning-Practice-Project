b3f5ad3a1dde24f20b9d178db26d4cab2538efda
XCC <873107451@qq.com>
2025-03-29 13:49:22 +0800

    commit

--- CIFAR10_ViT/models/ViT.py
+++ CIFAR10_ViT/models/ViT.py
@@ -4,13 +4,75 @@
 # @File    : ViT.py
 # @Software: PyCharm
 
+from functools import partial
+from collections import OrderedDict
 import torch
-from torch import nn, einsum
-import torch.nn.functional as F
-from einops import rearrange, repeat
+import torch.nn as nn
 
-# 判断t是否是元组，如果是，直接返回t；如果不是，则将t复制为元组(t, t)再返回。
-# 用来处理当给出的图像尺寸或块尺寸是int类型（如224）时，直接返回为同值元组（如(224, 224)）。
-def pair(t):
-    return t if isinstance(t, tuple) else (t, t)
+def drop_path(x, drop_prob: float = 0., training: bool = False):
+    """
+    Drop paths (Stochastic Depth) per sample (when applied in main path of residual blocks).
+    This is the same as the DropConnect impl I created for EfficientNet, etc networks, however,
+    the original name is misleading as 'Drop Connect' is a different form of dropout in a separate paper...
+    See discussion: https://github.com/tensorflow/tpu/issues/494#issuecomment-532968956 ... I've opted for
+    changing the layer and argument names to 'drop path' rather than mix DropConnect as a layer name and use
+    'survival rate' as the argument.
+    """
+    if drop_prob == 0. or not training: # 如果 drop_prob 为0（不丢弃）或不在训练模式，直接返回输入 x。仅在训练时且 drop_prob > 0 时执行后续操作。
+        return x
+    keep_prob = 1 - drop_prob # keep_prob 是路径保留概率。
+    shape = (x.shape[0],) + (1,) * (x.ndim - 1) # 生成一个与输入 x 维度匹配的形状 shape
+    random_tensor = keep_prob + torch.rand(shape, dtype=x.dtype, device=x.device) # 生成一个与输入 x 形状相同的随机张量 random_tensor
+    random_tensor.floor_()
+    output = x.div(keep_prob) * random_tensor
+    return output
+
+class DropPath(nn.Module):
+    """
+    Drop paths (Stochastic Depth) per sample  (when applied in main path of residual blocks).
+    """
+    def __init__(self, drop_prob=None):
+        super(DropPath, self).__init__()
+        self.drop_prob = drop_prob
+
+    def forward(self, x):
+        return drop_path(x, self.drop_prob, self.training)
+
+class PatchEmbed(nn.Module):
+    """
+    2D Image to Patch Embedding
+    """
+    def __init__(self, img_size=224, patch_size=16, in_c=3, embed_dim=768, norm_layer=None):
+        super(PatchEmbed, self).__init__()
+        img_size = (img_size, img_size)
+        patch_size = (patch_size, patch_size)
+        self.img_size = img_size
+        self.patch_size = patch_size
+        self.grid_size = (img_size[0] // patch_size[0], img_size[1] // patch_size[1])
+        self.num_patches = self.grid_size[0] * self.grid_size[1]
+
+        self.proj = nn.Conv2d(in_c, embed_dim, kernel_size=patch_size, stride=patch_size)
+        self.norm = norm_layer(embed_dim) if norm_layer else None
+
+    def forward(self, x):
+        B, C, H, W = x.shape
+        assert H == self.img_size[0] and W == self.img_size[1], \
+            f"Input image size ({H}*{W}) doesn't match model ({self.img_size[0]}*{self.img_size[1]})."
+
+        # flatten: [B, C, H, W] -> [B, C, HW]
+        # transpose: [B, C, HW] -> [B, HW, C]
+        x = self.proj(x).flatten(2).transpose(1, 2)
+        x = self.norm(x)
+        return x
+
+class Attention(nn.Module):
+    def __init__(self,
+                 dim,
+                 num_heads=8,
+                 qkv_bias=False,
+                 qk_scale=None,
+                 attn_drop=0.,
+                 proj_drop=0.):
+        super(Attention, self).__init__()
+
 
